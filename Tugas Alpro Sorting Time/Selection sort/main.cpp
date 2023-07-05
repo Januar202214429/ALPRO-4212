0e29497d206c9fc058f3e0201cc3f12aaae51a54
Januar Rizky Adsyamsa <128155585+Januar202214429@users.noreply.github.com>
2023-07-05 08:20:37 +0700

    Add files via upload

---
+++ Tugas Alpro Sorting Time/Selection sort/main.cpp
@@ -0,0 +1,36 @@
+#include <iostream>
+#include <chrono>
+using namespace std;
+using namespace std::chrono;
+
+void selectionSort(int arr[], int n) {
+    for (int i = 0; i < n - 1; i++) {
+        int minIndex = i;
+        for (int j = i + 1; j < n; j++) {
+            if (arr[j] < arr[minIndex]) {
+                minIndex = j;
+            }
+        }
+        swap(arr[i], arr[minIndex]);
+    }
+}
+
+int main() {
+    int arr[] = {64, 25, 12, 22, 11};
+    int n = sizeof(arr) / sizeof(arr[0]);
+
+    auto start = high_resolution_clock::now();
+    selectionSort(arr, n);
+    auto stop = high_resolution_clock::now();
+    auto duration = duration_cast<microseconds>(stop - start);
+
+    cout << "Sorted array: ";
+    for (int i = 0; i < n; i++) {
+        cout << arr[i] << " ";
+    }
+    cout << endl;
+
+    cout << "Time taken by selection sort: " << duration.count() << " microseconds" << endl;
+
+    return 0;
+}
